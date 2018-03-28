00ea007a8d62590ab6ea314a3842c0b22a76df17
Lies Hadjadj <hadjadjl@F213-07.e-im2ag.ujf-grenoble.fr>
2018-03-28 11:14:13 +0200

    dataframe added

---
+++ src/data_structures/DataFrame.java
@@ -0,0 +1,19 @@
+package data_structures;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataFrame {
+	String[] index;
+	String[] label;
+	List<List> frame;
+	
+	
+	public DataFrame(String[]... tabs) {
+		frame = new ArrayList<>();
+		for (String[] arg : tabs) {
+			
+		}
+	}
+}
